8dcbb28bbf465e0fda31764b329ca6af57ae34a7
rdheadyii <rdheadyii@gmail.com>
2023-07-11 08:30:19 -0400

    added JS to add the functionality of color changing based on past/present/future and to have persistance of user input

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,23 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Function to run the JS once page has rendered
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // event listener to save time and user input to local storage
+  $(".saveBtn").on("click", function() {
+    var time = $(this).siblings(".hour").text();
+    var userInput = $(this).siblings(".description").val();
+
+    localStorage.setItem(time, userInput)
+  });
+
+// applies CSS by comapring current hour to the id number for hour
+  $(".time-block").each(function() {
+    var timeDay = dayjs().get("hour");
+    var timeNow = parseInt($(this).attr("id"));
+
+    if (timeNow > timeDay) {
+      $(this).addClass("future");
+    } else if (timeNow === timeDay) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+
+// grabs user input from localstorage and adds it to page
+  $(".hour").each(function() {
+    var hour = $(this).text();
+    var currInput = localStorage.getItem(hour);
+
+    if(currInput !== null) {
+      $(this).siblings(".description").val(currInput)
+    }
+  });
+
+  // displays current date to page
+  $("#currentDay").text(dayjs().format("dddd, MMMM D YYYY"));
 });
